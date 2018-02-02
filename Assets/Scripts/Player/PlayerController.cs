8dd8e843aa463979508615436b587edaf5fe5474
Markus Nordin <markus.nordin@falun.ntig.se>
2018-02-02 11:09:59 +0100

    Del 2

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    float moveSpeed = 10f;
+    float jumpForce = 100f;
+    float movement;
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movement = Input.GetAxis("Horizontal");
+        
+	}
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(movement * moveSpeed, rb.velocity.y);
+    }
+}
