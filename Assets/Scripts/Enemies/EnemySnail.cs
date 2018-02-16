268f56ccd0cb2534a72aef826b43e0162d89b9e1
Markus Nordin <markus.nordin@falun.ntig.se>
2018-02-16 10:54:34 +0100

    Basic AI

---
+++ Assets/Scripts/Enemies/EnemySnail.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySnail : MonoBehaviour {
+
+    [SerializeField]
+    float moveSpeed = 1f;
+
+    [SerializeField]
+    Transform groundCheck;
+
+    bool movingLeft = true;
+    Vector2 direction;
+	
+	// Update is called once per frame
+	void Update () {
+
+        // TERNARY
+        direction = movingLeft ? Vector2.left : Vector2.right;
+
+        //if (movingLeft)
+        //    direction = Vector2.left;
+        //else
+        //    direction = Vector2.right;
+
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+
+        RaycastHit2D hit = Physics2D.Raycast(groundCheck.position, Vector2.down, 1f);
+        
+
+        if (hit.collider == false)
+        {
+            if (movingLeft)
+                transform.localScale = new Vector3(-1, 1, 1);
+            else
+                transform.localScale = new Vector3(1, 1, 1);
+
+            movingLeft = !movingLeft;
+        }
+	}
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
+
+    [SerializeField]
+    Text collectedText;
 
     List<GameObject> starList;
     int collectedStars = 0;
@@ -12,11 +16,14 @@ public class GameController : MonoBehaviour {
         starList = new List<GameObject>();
         starList.AddRange(GameObject.FindGameObjectsWithTag("Star"));
 
+        collectedText.text = "Stars Collected: 0";
 	}
 
     public void CollectedStar()
     {
         collectedStars++;
+
+        collectedText.text = "Stars Collected: " + collectedStars;
         
         if (collectedStars == starList.Count)
         {
